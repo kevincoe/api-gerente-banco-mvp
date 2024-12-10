6b3a7f40513a0c67c4d7690d7d44b6afc5f6e459
Kevin Coe <kevin.coe@g4flex.com.br>
2024-12-10 20:33:54 -0300

    validação id banco de dados não nulo e unico

--- models/cliente.py
+++ models/cliente.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()
 
 class Cliente(db.Model):
     __tablename__ = 'clientes'
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    nome = db.Column(db.String(100), nullable=False,)
     agencia = db.Column(db.String(10), nullable=False)
     conta = db.Column(db.String(20), nullable=False)
     nivel = db.Column(db.String(20), nullable=False)
